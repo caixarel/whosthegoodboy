cc90d43d21b8858f021286dbe55c4ec0ca863e7c
Bruno Godinho <godinho_bruno@hotmail.com>
2022-01-29 18:14:13 +0100

    working on offer create

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,32 @@
 class OffersController < ApplicationController
+  def index
+    @pet = Pet.find(params[:pet_id])
+    #@offers = @pet.offers
+  end
+
+  def Show
+  end
+
+  def new
+    @pet = Pet.find(params[:pet_id])
+    @offer = Offer.new
+  end
+  def create
+    @pet = Pet.find(params[:pet_id])
+    @offer = Offer.new(offer_params)
+    # price = params[]
+
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:starting_date, :end_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :pets do
+    resources :offers
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
