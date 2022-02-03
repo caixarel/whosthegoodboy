b5f5550c75fc9e2b2f0177934e7017686a48593f
Helene Delachapelle <helene.delachapelle@edhec.com>
2022-02-03 19:44:09 +0100

    pets_controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,31 @@
 class PetsController < ApplicationController
+
+  def index
+    @pets = Pet.all
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pets_params)
+
+    if @pet.save
+      redirect_to pets_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pets_params
+    params.require(:pet).permit(:name, :address, :price_per_hour, :description, :category)
+  end
+
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,9 @@
 class Pet < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :price_per_hour, presence: true
 end
