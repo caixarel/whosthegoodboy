39dd5d2c5c5c4506bca7c7bab3173a3b2a1b8689
Helene Delachapelle <helene.delachapelle@edhec.com>
2022-02-05 14:34:15 +0100

    updated with master merge

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,5 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:edit, :update, :destroy, :show]
 
   def index
     @pets = Pet.all.order(:id)
@@ -18,13 +19,8 @@ class PetsController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
-    @pet = Pet.new(pets_params)
-
-=======
     @pet = Pet.new(pet_params)
     @pet.user = current_user
->>>>>>> master
     if @pet.save
       redirect_to pets_path
     else
@@ -32,24 +28,10 @@ class PetsController < ApplicationController
     end
   end
 
-<<<<<<< HEAD
-  private
-
-  def set_pet
-    @pet = Pet.find(params[:id])
-  end
-
-  def pets_params
-    params.require(:pet).permit(:name, :address, :price_per_hour, :description, :category)
-  end
-
-=======
   def edit
-    @pet = Pet.find(params[:id])
   end
 
   def update
-    @pet = Pet.find(params[:id])
     if @pet.user == current_user
       if @pet.update(pet_params)
         redirect_to pet_path(@pet)
@@ -60,19 +42,20 @@ class PetsController < ApplicationController
   end
 
   def destroy
-    @pet = Pet.find(params[:id])
     if @pet.user == current_user
       @pet.destroy
     end
   end
 
   def show
-    @pet = Pet.find(params[:id])
   end
   private
 
-  def pet_params
-    params.require(:pet).permit(:address, :name,:description,:category,:price_per_hour, :photo)
+  def set_pet
+    @pet = Pet.find(params[:id])
   end
->>>>>>> master
+
+  def pets_params
+    params.require(:pet).permit(:name, :address, :price_per_hour, :description, :category)
+  end
 end
